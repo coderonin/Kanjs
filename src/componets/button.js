2c44d00a40c632d1fd50f6cf1e53ba49392e1cc2
Claudio Bernal <claudio.mattioli@globant.com>
2017-02-06 15:07:36 -0300

    Create base classes and component base definition

---
+++ src/componets/button.js
@@ -0,0 +1,17 @@
+Kan.define("KanButton",{
+    alias: "kbutton",
+    tpl: "<button>{children}</button>",
+    cls: "k-button",
+    events: ['click'],
+    listeners: {
+        click: function(cmp, builder, event){
+            console.log("internally components could do some logic before actually trigger an event");
+            builder.addCls(cmp, "x-focus");
+            builder.trigger("click", cmp, event);
+        },
+        blur: function(cmp, builder, event){
+            builder.removeCls(cmp, "x-focus");
+            builder.trigger("blur", cmp, event);
+        }
+    }
+});
